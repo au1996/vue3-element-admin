8e668557ebaba69ced5aa5952a7cb2ad3b1cbfab
au1996 <15119376155@163.com>
2021-07-01 22:03:43 +0800

    feat: :rocket: add dist dir

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ npm run build
 
 # 部署
 git add .
-git commit -m 'feat: :pushpin: Fixed version number'
+git commit -m 'feat: :rocket: add dist dir'
 git push origin master
 git subtree push --prefix dist origin gh-pages
 
---
+++ dist/assets/menu11.300e39be.js
@@ -0,0 +1,1 @@
+import{b as s,W as e,c as a,l as t}from"./vue.4d80af16.js";const o={expose:[],setup(o){const u=s("菜单1-1");return(s,o)=>(e(),a("h1",null,t(u.value),1))}};export default o;
