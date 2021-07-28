d2b7a4ffd2a66409c1ba413dab0c34ca9f991763
au1996 <15119376155@163.com>
2021-07-28 21:02:06 +0800

    feat: :seedling: add directive

---
+++ assets/menu12.13e35f98.js
@@ -0,0 +1,1 @@
+import{b as s,W as e,c as t,l as a}from"./vue.0c21260b.js";const o={expose:[],setup(o){const u=s("菜单1-2");return(s,o)=>(e(),t("h1",null,a(u.value),1))}};export default o;
--- assets/menu2.fb302129.js
+++
@@ -1,1 +0,0 @@
-import{b as s,W as e,c as t,l as a}from"./vue.0f0cc622.js";const o={expose:[],setup(o){const u=s("菜单2");return(s,o)=>(e(),t("h1",null,a(u.value),1))}};export default o;
