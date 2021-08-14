2b195359754859b8a9a05c7a9cc0e070dbde9052
au1996 <15119376155@163.com>
2021-08-14 16:16:23 +0800

    feat: :zap: update vue version

---
+++ assets/index.72296584.js
@@ -0,0 +1,1 @@
+import{a7 as a,a8 as s,o as e,c as t,a6 as c,a as o}from"./vue.4b0dfa6f.js";const d={},r=c();a("data-v-20172415");const f={class:"xueyueView"},i=o("h1",{class:"title"},"雪月测试 scss 全局变量",-1);s();const l=r(((a,s)=>(e(),t("div",f,[i]))));d.render=l,d.__scopeId="data-v-20172415";export{d as default};
