6057ae45543159aa99507c7055670c10ed5b3de7
gujinzhu <15119376155@163.com>
2021-11-29 15:25:32 +0800

    feat: :zap: update stylelintrc

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ npm run build
 
 # 部署
 git add .
-git commit -m 'feat: :zap: update vue version'
+git commit -m 'feat: :zap: update stylelintrc'
 git push origin master
 git subtree push --prefix dist origin gh-pages
 
