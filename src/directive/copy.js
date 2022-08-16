1e88154c419e61fa5e3b38ec1f27cc691d61d9f1
gujinzhu <15119376155@163.com>
2022-08-16 17:20:59 +0800

    chore: update copy.js

--- src/directive/copy.js
+++ src/directive/copy.js
@@ -5,42 +5,36 @@ export default {
     el.$value = value
     el.handler = () => {
       if (!el.$value) {
-        // 值为空的时候，给出提示。可根据项目UI设计
-        ElMessage({
-          type: 'error',
-          message: '内容为空'
-        })
+        ElMessage.error('内容为空')
         return
       }
-      // 动态创建 textarea 标签
+
+      let content = el.$value
+      if (typeof el.$value === 'function') {
+        content = el.$value()
+      }
+
       const textarea = document.createElement('textarea')
-      // 将该 textarea 设为 readonly 防止 iOS 下自动唤起键盘，同时将 textarea 移出可视区域
       textarea.readOnly = 'readonly'
       textarea.style.position = 'absolute'
       textarea.style.left = '-9999px'
-      // 将要 copy 的值赋给 textarea 标签的 value 属性
-      textarea.value = el.$value
-      // 将 textarea 插入到 body 中
+      textarea.value = content
       document.body.appendChild(textarea)
-      // 选中值并复制
       textarea.select()
       const result = document.execCommand('Copy')
+
       if (result) {
-        ElMessage({
-          type: 'success',
-          message: '复制成功：' + el.$value
-        })
+        ElMessage.success('复制成功：' + content)
       }
+
       document.body.removeChild(textarea)
     }
-    // 绑定点击事件，就是所谓的一键 copy 啦
+
     el.addEventListener('click', el.handler)
   },
-  // 当传进来的值更新的时候触发
   updated(el, { value }) {
     el.$value = value
   },
-  // 指令与元素解绑的时候，移除事件绑定
   unmounted(el) {
     el.removeEventListener('click', el.handler)
   }
